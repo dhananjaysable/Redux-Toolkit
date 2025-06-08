b13d528ddabb3161c81cd6ba6edd89a69f5cd43c
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-06-08 14:31:17 +0530

    redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./store/slice/counterSlice";
+import { decrement, increment, reset } from "./store/slice/counterSlice";
 
 const App = () => {
   const dispatch = useDispatch();
-  const count = useSelector((state) => state.count);
+  const count = useSelector((state) => state.count.count);
   return (
     <div>
       <h1>Count: {count}</h1>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
     </div>
   );
 };
--- src/store/slice/counterSlice.js
+++ src/store/slice/counterSlice.js
@@ -1,17 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+const initialState = {
+    count: 0
+}
 const counterSlice = createSlice({
     name: "counter",
-    initialState: 0,
+    initialState,
     reducers: {
         increment: (state) => {
-            return state + 1
+            state.count += 1
         },
         decrement: (state) => {
-            return state - 1
+            state.count -= 1
+        },
+        reset: (state) => {
+            state.count = initialState.count
         }
     }
 })
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, reset } = counterSlice.actions;
 export default counterSlice.reducer;
